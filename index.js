a8d7698c2540fb7fba147107eeb4dfcec17647e8
James Overbay <orangetoque13@gmail.com>
2018-01-10 06:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ function removeFromCart(item) {
  
 
 function placeOrder(cardNumber) {
-  console.log("Sorry, we don't have a credit card on file for you.")
+  console.log("Sorry we don't have a credit card on file for you.")
 }
