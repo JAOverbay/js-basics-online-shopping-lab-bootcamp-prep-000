64c824f882e904634bcfce03107692385ef05c80
James Overbay <orangetoque13@gmail.com>
2018-01-10 06:47:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
+  var itemInCart = false
   for (let i = 0; i<cart.length; i++) {
     if (cart[i].hasOwnProperty(item)){
       itemInCart = true;
